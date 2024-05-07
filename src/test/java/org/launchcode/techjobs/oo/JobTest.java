63bc64ed95a11a543a365f18af3a301c7eca0ad1
molalenim <molaleni.mabaso@gmail.com>
2024-05-07 15:51:52 -0400

    TASK 4: USE UNIT TESTING TO VERIFY PARTS OF THE JOB CLASS

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,49 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.testng.Assert.assertNotEquals;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        //Arrange
+        String msg = "to verify that the IDs of the two objects are distinct.";
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        //Assert
+        assertNotEquals(job1.getId(), job2.getId(), msg);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        // Arrange
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        // Assert
+        assertTrue(job instanceof Job);
+        assertEquals("Product tester", job.getName());
+        assertEquals("ACME", job.getEmployer().getValue());
+        assertEquals("Desert", job.getLocation().getValue());
+        assertEquals("Quality control", job.getPositionType().getValue());
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        // Arrange
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        // Act
+        boolean result = job1.equals(job2);
+
+        // Assert
+        assertFalse(result);
+    }
+
+
 }
